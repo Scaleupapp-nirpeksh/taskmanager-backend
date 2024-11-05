15dfa572b03187ffe67cd8adc983fb5865f32a00
Scaleupapp <144357944+Scaleupapp@users.noreply.github.com>
2024-11-05 12:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ cron.schedule('0 20 * * *', async () => {
 });
 
 // Server setup
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
